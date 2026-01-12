517ed9f325ef629ef815b30d81f2bc86fd4b5e4a
Filipe Souto <filipesoutocosta@gmail.com>
2026-01-12 07:08:52 -0300

    editing

--- angular-blog/src/app/pages/home/home.ts
+++ angular-blog/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainTitle } from '../../components/main-title/main-title';
 import { BigNews } from '../../components/big-news/big-news';
+import { SmallNews } from '../../components/small-news/small-news';
 
 @Component({
   selector: 'app-home',
-  imports: [MainTitle, BigNews],
+  imports: [MainTitle, BigNews, SmallNews],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
